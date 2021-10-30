5edff251cd6f5c03e7784826c900e71cabb373b3
dartaryan <76628877+dartaryan@users.noreply.github.com>
2021-10-30 05:05:31 +0300

    create Authenticator Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { environment } from 'src/environments/environment';
 import { FirebaseTSApp } from 'firebasets/firebasetsApp/FirebaseTSApp';
+import { HomeComponent } from './pages/home/home.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatCardModule } from '@angular/material/card';
+import { AuthenticatorComponent } from './tools/authenticator/authenticator.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HomeComponent, AuthenticatorComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatBottomSheetModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
